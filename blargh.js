c87a24f37f5fd9bd014a3119cec388c8878c4d83
Blake Hobbs <believeinthefuture@outlook.com>
2018-10-25 23:33:32 -0700

    should be 1st commit

---
+++ blargh.js
@@ -0,0 +1,16 @@
+//10.25.2018
+//based:https://mathiasbynens.be/notes/es6-const
+
+const myFunc = {};
+myFunc.bar = 42;
+var firstProj = document.getElementById("Item01");
+
+//To make an object’s values immutable, use Object.freeze(). It has been around since ES5 and is widely available nowadays.
+
+const fork = Object.freeze({
+	'bar' : 27
+});
+fork.bar = 42;
+console.log(fork.bar);
+
+//this way there is a 'sloppy' error that at least shows something
